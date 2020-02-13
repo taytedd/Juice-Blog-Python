01b6779aaf6b76a4b5a049fa96f47542d02af2e8
taytedd <taytedd@gmail.com>
2020-02-13 16:35:54 +0300

    CRUD on post user check on edit and delete

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,57 @@
 from django.shortcuts import render
 from .models import Post
-posts = [
-    {
-        'author': 'Ted Ochingo',
-        'title': 'Blog Post',
-        'content': 'We The Best',
-        'date_posted': 'Jun 20, 2011'
-    },
-    {
-        'author': 'Key Glock',
-        'title': 'Blog Post',
-        'content': 'We The Best',
-        'date_posted': 'May 2, 2015'
-    }
-]
-
+from django.views.generic import ListView, DetailView, CreateView,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
